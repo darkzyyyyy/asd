daaa86941215ff8d8c5c42b2602e8e2d214846b0
Руслан <@WIN-F52HFKED8TO>
2022-03-07 22:35:45 +0300

    Добавьте файлы проекта.

---
+++ App6/App6/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using CustomCalendarControl.Views;
+using Rg.Plugins.Popup.Services;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App6
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        private async void Button_Clicked(object sender, System.EventArgs e)
+        {
+            var calendarPage = new CalendarPage(DateTime.Now, null);
+            calendarPage.SelectedDateCommand = new Command<DateTime>((item) =>
+            {
+                //Perform operation on selected Date
+            });
+            await PopupNavigation.Instance.PushAsync(calendarPage);
+        }
+    }
+}
